64cd7930a42a90ad6b717cf89972769da2a2dbee
Danylo Hlushko <danylo.hlushko.dev@gmail.com>
2024-05-06 14:54:37 +0300

    init

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Heyyyyyy</h1>");
+});
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
