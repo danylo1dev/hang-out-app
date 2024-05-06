952bea8685a0df1473fbee22bb21e3cffdaf618a
Danylo Hlushko <danylo.hlushko.dev@gmail.com>
2024-05-06 15:45:10 +0300

    brodcast messages

--- client/script.js
+++ client/script.js
@@ -1,5 +1,43 @@
 const socket = io()
 
+const messaging = (msgsArea, name, msg) => {
+    const item = document.createElement('li')
+    item.textContent = `${name}: ${msg}`
+    msgsArea.appendChild(item)
+    window.scrollTo(0, document.body.scrollHeight)
+}
+
+const displayConnectionStatus = (nickName) => {
+    const connectionStatus = document.getElementById('connectionStatus')
+    connectionStatus.innerText = `Hello ${nickName} you connected Successfully`
+}
+
 socket.on('connect', () => {
+    socket.nickName = prompt('Enter Your Name')
+    displayConnectionStatus(socket.nickName)
+    socket.emit('new user', socket.nickName)
     console.log(`A user connected with id ${socket.id}`)
+    const messages = document.getElementById('messages')
+    const form = document.getElementById('grpMsgForm')
+    const input = document.getElementById('grpMsgInput')
+
+    form.onsubmit = (e) => {
+        e.preventDefault()
+        if(input.value) {
+            const msg = input.value
+            socket.emit('group message', socket.nickName, msg)
+        }
+        input.value = ''
+    }
+    socket.on('new user', nickName => {
+        alert(`${nickName} Has joined the Chat`)
+    })
+    socket.on('group message', (nickName, msg) => {
+        messaging(messages, nickName, msg)
+    })
+    
+    socket.on('user left', userNickName => {
+        alert(`${userNickName} Has left the chat`)
+    })
 })
+
--- server.js
+++ server.js
@@ -1,21 +1,33 @@
-const express = require("express");
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-const port = 3000;
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const {Server} = require('socket.io')
+const io = new Server(server)
+const port = 3000
 
-app.use(express.static("client"));
+app.use(express.static('client'))
 
-io.on("connection", (socket) => {
-  console.log(`A user connected with id ${socket.id}`);
+io.on('connection', socket => {
+    console.log(`A user connected with id ${socket.id}`)
 
-  socket.on("disconnect", () => {
-    console.log(`User with id ${socket.id} Disconnected`);
-  });
-});
+    socket.on('new user', nickName => {
+        socket.nickName = nickName
+        console.log(`${nickName} has connected, his id is: ${socket.id}`)
+        socket.broadcast.emit('new user', nickName)
+    })
+
+    socket.on('group message', (nickName ,msg) => {
+        console.log(msg)
+        io.emit('group message', nickName, msg)
+    })
+
+    socket.on('disconnect', () => {
+        console.log(`User with id ${socket.id} Disconnected`)
+        socket.broadcast.emit('user left', socket.nickName)
+    })
+})
 
 server.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+    console.log(`Listening on port ${port}`)
+})
